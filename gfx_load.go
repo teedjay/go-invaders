96c3baf9ac27ca8f76670efa464ed8f410575a46
Thore Johnsen <thore@jpro.no>
2026-02-07 21:41:50 +0100

    Cleanup

--- gfx_load.go
+++ gfx_load.go
@@ -65,23 +65,23 @@ func loadC64Sprites() ([][]*ebiten.Image, error) {
 }
 
 func loadPlayerSprites() (PlayerSpriteSet, error) {
-	m, err := loadImage("/Users/thorej/opt/codex/go-invaders/assets/Player/player_b_m.png")
+	m, err := loadImage("/Users/thorej/opt/codex/go-invaders/assets/player/player_b_m.png")
 	if err != nil {
 		return PlayerSpriteSet{}, err
 	}
-	l1, err := loadImage("/Users/thorej/opt/codex/go-invaders/assets/Player/player_b_l1.png")
+	l1, err := loadImage("/Users/thorej/opt/codex/go-invaders/assets/player/player_b_l1.png")
 	if err != nil {
 		return PlayerSpriteSet{}, err
 	}
-	l2, err := loadImage("/Users/thorej/opt/codex/go-invaders/assets/Player/player_b_l2.png")
+	l2, err := loadImage("/Users/thorej/opt/codex/go-invaders/assets/player/player_b_l2.png")
 	if err != nil {
 		return PlayerSpriteSet{}, err
 	}
-	r1, err := loadImage("/Users/thorej/opt/codex/go-invaders/assets/Player/player_b_r1.png")
+	r1, err := loadImage("/Users/thorej/opt/codex/go-invaders/assets/player/player_b_r1.png")
 	if err != nil {
 		return PlayerSpriteSet{}, err
 	}
-	r2, err := loadImage("/Users/thorej/opt/codex/go-invaders/assets/Player/player_b_r2.png")
+	r2, err := loadImage("/Users/thorej/opt/codex/go-invaders/assets/player/player_b_r2.png")
 	if err != nil {
 		return PlayerSpriteSet{}, err
 	}
@@ -89,23 +89,23 @@ func loadPlayerSprites() (PlayerSpriteSet, error) {
 }
 
 func loadPlayerSuperSprites() (PlayerSpriteSet, error) {
-	m, err := loadImage("/Users/thorej/opt/codex/go-invaders/assets/Player/player_r_m.png")
+	m, err := loadImage("/Users/thorej/opt/codex/go-invaders/assets/player/player_r_m.png")
 	if err != nil {
 		return PlayerSpriteSet{}, err
 	}
-	l1, err := loadImage("/Users/thorej/opt/codex/go-invaders/assets/Player/player_r_l1.png")
+	l1, err := loadImage("/Users/thorej/opt/codex/go-invaders/assets/player/player_r_l1.png")
 	if err != nil {
 		return PlayerSpriteSet{}, err
 	}
-	l2, err := loadImage("/Users/thorej/opt/codex/go-invaders/assets/Player/player_r_l2.png")
+	l2, err := loadImage("/Users/thorej/opt/codex/go-invaders/assets/player/player_r_l2.png")
 	if err != nil {
 		return PlayerSpriteSet{}, err
 	}
-	r1, err := loadImage("/Users/thorej/opt/codex/go-invaders/assets/Player/player_r_r1.png")
+	r1, err := loadImage("/Users/thorej/opt/codex/go-invaders/assets/player/player_r_r1.png")
 	if err != nil {
 		return PlayerSpriteSet{}, err
 	}
-	r2, err := loadImage("/Users/thorej/opt/codex/go-invaders/assets/Player/player_r_r2.png")
+	r2, err := loadImage("/Users/thorej/opt/codex/go-invaders/assets/player/player_r_r2.png")
 	if err != nil {
 		return PlayerSpriteSet{}, err
 	}
